a15084b38a103603b60d458c4f7b83b14e65417b
Andrew Watson <eearw@leeds.ac.uk>
2022-10-19 13:18:02 +0100

    New branch for adding error scaling

---
+++ util/scale_vstd.m
@@ -0,0 +1,18 @@
+function vstd_scale = scale_vstd(vstd)
+%=================================================================
+% function vstd_scale()
+%-----------------------------------------------------------------
+% Scale InSAR velocity uncertainties using semivariogram to mitigate the
+% impact of the reference pixel.
+%                                                                  
+% INPUT:                                                           
+%   par: parameter structure from readparfile.
+%
+% OUTPUT:    
+%   vstd_scale: scaled uncertainty
+%   
+% Andrew Watson     19-10-2022
+%                                                                  
+%=================================================================
+
+end
